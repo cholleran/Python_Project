6d8e21e4beb717f06d333d957a9eb8b2e3e4df0d
cholleran <36338454+cholleran@users.noreply.github.com>
2018-04-15 21:47:23 +0100

    Add files via upload

--- iris.project.py
+++ iris.project.py
@@ -3,8 +3,11 @@
 # The code on line 7 asks pandas to read the file and create a dataframe.
 # The dataframe is a structured grid/array which prepares the data for analysis.
 # The pandas library is already installed with Anaconda.
+# Imported numpy and matplotlib which are the main packages for visualising data in Python.
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Adding column names: https://chrisalbon.com/python/data_wrangling/pandas_dataframe_importing_csv/
 dataframe = pd.read_csv("C:\\Users\MyComputer\Desktop\projects\Data\iris.csv", names=['Sl_Length', 'Sl_width', 'Pl_Length', 'Pl Width', 'Class'])
@@ -15,8 +18,9 @@ print(dataframe.to_string())
 print(dataframe.describe())
 
 #The below line of code creates a new object containing the data organized by species.https://chrisalbon.com/python/data_wrangling/pandas_apply_operations_to_groups/
-grouped = dataframe.groupby("Class")
+grouped = dataframe.groupby("Class").hist()
 
 
 # It is now possible to calculate the mean for each individual species.
-print(grouped.mean())
+print(grouped)
+
